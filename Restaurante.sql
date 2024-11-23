37228cf1d2e0176c9de4e5dfd92c41e7c013800f
Rhayssauv <161035203+Rhayssauv@users.noreply.github.com>
2024-11-23 17:57:44 -0300

    Add files via upload

--- Restaurante.sql
+++ Restaurante.sql
@@ -210,3 +210,18 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- Função
+
+DELIMITER $$
+
+CREATE FUNCTION calcular_desconto(valor DECIMAL(10,2)) 
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE desconto DECIMAL(10,2);
+    SET desconto = valor * 0.10;
+    RETURN valor - desconto;
+END$$
+
+DELIMITER ;
